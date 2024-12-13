fc771382b00f38c71d83113e940cce8653a209d2
marslikbola <mengboyevfarrux5@gmail.com>
2024-12-13 16:06:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let nom = prompt("ismingizni kiriting")
+let nom1 = prompt("yoshni kirit")
+let nom2 = prompt(" qayerliksan")
+let nom3 = prompt(" familiyangni kirit")
+
+
+const user ={
+    name: nom ,
+    age:nom1,
+    city :nom2,
+    surname : nom3,
+}
+
+
+console.log(user);
